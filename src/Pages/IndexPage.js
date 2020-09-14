696d5190496ed16b7079e375a84786b39cdf9a08
ianArlen <ian.street.fut@gmail.com>
2020-09-13 19:05:48 -0500

    Primer commit

---
+++ src/Pages/IndexPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const IndexPage = (props) => {
+  
+  React.useEffect(() => {
+    
+    const token = localStorage.getItem("CC_Token");
+    
+    
+ 
+    if (!token) {
+    
+      props.history.push("/login");
+    
+    } else {
+      props.history.push("/dashboard");
+    }
+
+  }, [0]);
+  return <div></div>;
+};
+
+
+export default IndexPage;
